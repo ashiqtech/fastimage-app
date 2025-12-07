b9990e1dee93eddf78c52fa12e199afb99756489
ashiqtech <cryptodrop077@gmail.com>
2025-12-07 17:59:01 +0500

    Create App.js

---
+++ client/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageGenerator from './components/ImageGenerator';
+import './App.css'; // Agar koi global styles ho toh
+
+function App() {
+  return (
+    <div className="App">
+      <ImageGenerator />
+    </div>
+  );
+}
+
+export default App;
